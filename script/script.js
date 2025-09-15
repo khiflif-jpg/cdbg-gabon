f2149c1024ff00f99befcacbfc0f8a57de7395b6
khiflif-jpg <khiflif@gmail.com>
2025-09-15 20:50:14 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,12 +2,9 @@
 document.querySelectorAll('nav a').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-    const targetID = this.getAttribute('href');
-    const targetElement = document.querySelector(targetID);
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: 'smooth'
-      });
-    }
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
   });
 });
