bf63d29fc0f6dffd7109ba93fb30f6aabf772e2b
khiflif-jpg <khiflif@gmail.com>
2025-09-16 03:59:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.querySelector('form').addEventListener('submit', function(e) {
+    const nom = document.getElementById('nom').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const sujet = document.getElementById('sujet').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    if (!nom || !email || !sujet || !message) {
+        alert('Merci de remplir tous les champs.');
+        e.preventDefault();
+    }
+});
 
