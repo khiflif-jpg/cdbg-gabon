b36fd8be65da3946b7900b1047d9012dbdaf53a5
khiflif-jpg <khiflif@gmail.com>
2025-11-09 09:01:22 +0100

    Create get-rss-pfbc.php

---
+++ get-rss-pfbc.php
@@ -0,0 +1,2 @@
+const RSS_URL_OVERRIDE_1 = "https://www.cdbg-gabon.com/get-rss-pfbc.php";  // PFBC via proxy
+const RSS_URL_OVERRIDE_2 = "https://rss.app/feeds/NbpOTwjyYzdutyWP.xml";  // ATIBT direct
